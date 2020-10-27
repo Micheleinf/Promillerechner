2c414f79e3d31d5b9ce9a5386ed04f4cd2a132e5
Michele <54061875+Micheleinf@users.noreply.github.com>
2020-10-27 17:58:12 +0100

    Create AlkoholischesGetraenk.java

---
+++ AlkoholischesGetraenk.java
@@ -0,0 +1,66 @@
+//AlkoholischesGetraenk.java from package Promillerechner
+//Michele Biondi
+//Last updated 21.10.2020
+
+
+class AlkoholischesGetraenk {
+    
+    private int volumen;
+    private double alkoholgehalt;
+    private double getrunkenVor;
+    
+    
+    public double BierAlkoholGehalt = 0.05;
+    public double WeinAlkoholGehalt = 0.10;
+    public double SchnappsAlkoholGehalt = 0.40;
+    
+    
+    //Variablen
+    
+    /**
+     * 
+     * @return 
+     */
+    private int volumenInMilliter(){
+        return 0;
+    
+    }
+    
+    private double alkoholgehalt(){
+        return 0;
+    
+    }
+    
+     private java.util.Date getrunkenAm(){
+        return null;
+    }
+     
+   
+    
+     
+     public AlkoholischesGetraenk(int volumen, double alkoholgehalt, double getrunkenVor){
+        this.volumen = volumen;
+        this.alkoholgehalt = alkoholgehalt;
+        this.getrunkenVor = getrunkenVor;
+     
+     }
+
+    public double getStundenSeitEinnahme(java.util.Date jetzt) {
+        return 0;
+    }
+     
+     public double getAlkoholMasseInGramm(){
+        return 0;
+     }
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
