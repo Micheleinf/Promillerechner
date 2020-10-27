04c7fc8b2eb33bc9433792db7d643c6ca2962ff0
Michele <54061875+Micheleinf@users.noreply.github.com>
2020-10-27 17:55:52 +0100

    Create Start.java

---
+++ Start.java
@@ -0,0 +1,14 @@
+//Start.java from package Promillerechner
+//Michele Biondi
+//Last updated 21.10.2020
+//Starter Promillerechner
+
+public class Start {
+
+    
+    public static void main(String[] args) {
+        
+        new GUI().promilleRechner();
+    }
+    
+}
